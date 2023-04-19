704d55feacb19a82313c05acd8303e6ba397749d
liisit <ltaimre@tlu.ee>
2023-04-20 01:29:40 +0300

    added functionality

--- src/index.js
+++ src/index.js
@@ -1,26 +1,99 @@
-
-//DOCS
-// https://docs.directus.io/extensions/endpoints.html
-// https://docs.directus.io/extensions/creating-extensions.html
-// re
-
-//returns relations data - many_collection by one_field
 export default {
   id: "api",
-  handler: (router, { services, exceptions }) => {
+  handler: async (router, { services, exceptions }) => {
     const { ItemsService } = services;
-    router.get("/", (req, res) => res.send("directus-custom-api"));
-    router.get("/:field", async function (req, res, next) {
+
+    router.get("/:field", async (req, res, next) => {
+      const {
+        params: { field },
+        query: {  subfield = [], lang},
+        schema,
+        accountability,
+      } = req;
+
+      if (!subfield) {
+        return res.status(400).json({
+          error: "Missing required parameter: subfield",
+        });
+      }
+
+      if (!lang) {
+        return res.status(400).json({
+          error: "Missing required parameter: language",
+        });
+      }
+
       const relationsService = new ItemsService("directus_relations", {
-        schema: req.schema,
-        accountability: req.accountability,
+        schema,
+        accountability,
       });
-      const field = req.params.field
-      const relationData = await relationsService.readByQuery({
-        filter: { one_field: { _eq: field } },
+
+      const fieldsService = new ItemsService("directus_fields", {
+        schema,
+        accountability,
       });
-      const manyCollection = relationData && relationData[0] ? relationData[0].many_collection : null
-      res.send(manyCollection)
+      const subfieldArray = Array.isArray(subfield) ? subfield : [subfield];
+
+
+      // Get the collection data for a given collection
+      async function getCollectionData(collection) {
+        const collectionData = await fieldsService.readByQuery({
+          filter: { collection: { _eq: collection } },
+        });
+        return collectionData.map(({ field }) => field);
+      }
+
+      // Get the many collection for a given field
+      async function getManyCollection(field) {
+        const relationData = await relationsService.readByQuery({
+          filter: { one_field: { _eq: field } },
+        });
+        return relationData?.[0]?.many_collection;
+      }
+
+      // Get the translation for a given language
+      function getTranslation(translations, lang, sub) {
+        const labelObj = translations.filter((tr) => tr.language === lang);
+        const returnLabel = labelObj && labelObj[0] ? labelObj[0].translation : sub
+        return returnLabel
+      }
+
+      const dateTimeFields = await getCollectionData("date_time");
+
+      const labelArray = [];
+      for (const sub of subfieldArray) {
+        if (sub === "translations") {
+          const collectionData = await fieldsService.readByQuery({
+            filter: { collection: { _eq: "options" } },
+          });
+          const { translations } = collectionData.find(
+              ({ field }) => field === sub
+          ) || {};
+          const label = getTranslation(translations, lang);
+          const obj = { [sub]: label };
+          labelArray.push(obj);
+        } else if (dateTimeFields.includes(sub)) {
+          const { translations } =
+          dateTimeFields.find(({ field }) => field === sub) || {};
+          const label = getTranslation(translations, lang);
+          const obj = { [sub]: label };
+          labelArray.push(obj);
+        } else {
+          const manyCollection = await getManyCollection(field);
+          const collectionData = await fieldsService.readByQuery({
+            filter: { collection: { _eq: manyCollection } },
+          });
+          const { translations } = collectionData.find(
+              ({ field }) => field === sub
+          ) || {};
+          const label = translations ? getTranslation(translations, lang) : null
+          const obj = { [sub]: label};
+          labelArray.push(obj);
+        }
+      }
+
+      res.send(labelArray);
+      next();
     });
   },
 };
